cd57bf7df1f7a8e730aeb84020c7a8612a27b64b
Mateusz Kardas <147061247+mkardas01@users.noreply.github.com>
2024-11-04 21:59:36 +0100

    use textoutputFormat

--- src/main/java/com/example/bigdata/AppRatingAggregator.java
+++ src/main/java/com/example/bigdata/AppRatingAggregator.java
@@ -11,6 +11,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -34,6 +35,8 @@ public class AppRatingAggregator extends Configured implements Tool {
         job.setJarByClass(this.getClass());
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setOutputFormatClass(TextOutputFormat.class);
 
         job.setMapperClass(AppMapper.class);
         job.setCombinerClass(AppCombiner.class);
---
+++ src/main/java/com/example/bigdata/script.sql
@@ -0,0 +1,43 @@
+-- Tabela dla input_dir3
+CREATE EXTERNAL TABLE IF NOT EXISTS data3 (
+    developer_id STRING,
+    release_year INT,
+    total_rating_sum DOUBLE,
+    total_rating_count INT,
+    total_app_count INT
+)
+    ROW FORMAT DELIMITED
+        FIELDS TERMINATED BY ';'
+    STORED AS TEXTFILE
+    LOCATION ${input_dir3};
+
+
+-- Tabela tymczasowa dla input_dir4
+CREATE EXTERNAL TABLE IF NOT EXISTS data4 (
+    developer_name STRING,
+    developer_website STRING,
+    developer_email STRING,
+    developer_id STRING
+)
+    ROW FORMAT DELIMITED
+        FIELDS TERMINATED BY '\u0001'
+    STORED AS TEXTFILE
+    LOCATION ${input_dir4};
+
+
+INSERT OVERWRITE DIRECTORY ${output_dir6}
+    ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.JsonSerDe'
+SELECT
+    d4.developer_name AS developer_name,
+    d3.release_year AS year,
+    d3.total_rating_sum / d3.total_app_count AS avg_rate,
+    d3.total_app_count AS count_apps,
+    d3.total_rating_count AS count_rates
+FROM
+    data3 d3
+        JOIN
+    data4 d4
+    ON
+        d3.developer_id = d4.developer_id
+ORDER BY avg_rate DESC
+LIMIT 3;
